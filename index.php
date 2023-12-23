c088bca5bfd099f7c54b0b616fd7333a7376f9bf
RubenRosalia <Ruben.Rosalia2002@gmail.com>
2023-12-23 01:40:11 +0100

    Removed Google Analytics

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
     <title>Ruben Rosalia</title>
     <script type="module" src="dist/js/main.min.js"></script>
     <link rel="stylesheet" href="dist/css/main.min.css">
-    
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFQPPCM2HR"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZFQPPCM2HR');
-</script>
 </head>
 <body>
 
